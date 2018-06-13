ad7dcfe8805617f0772677309bcfe19935137ef7
ryhan.chowdhury3@gmail.com <ryhan.chowdhury3@gmail.com>
2018-06-13 22:16:10 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
+  if array.length == 1 
+    return array[0]
+  
+  elsif array.length == 2 
+    return array.join(" and ")
+  
+  else
+    array[-1] = "and #{array[-1]}"
+    array = array.join(", ")
+    return array
+  end
 
 end
+
+poop = ["a", "b", "c"]
+puts oxford_comma(poop)
